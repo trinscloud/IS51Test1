fc71ee2ac2335d53605d0beb4120ecbd5db1197b
trinscloud <trinhamm@gmail.com>
2022-03-25 11:39:18 -0700

    finish structured english

--- test_1.py
+++ test_1.py
@@ -1,3 +1,20 @@
 from mimetypes import init
 
 
+"""
+
+The program will depict two options and determine which of the two options offer a greater pay rate. Option 1 offers $100 per day while option 2 offers incremental amounts (x2) over a certain time period (10 days)
+
+Option 1 will output 1000 because $100 * 10 days
+option 2 is a double multiplier over a 10 day period. 
+(1 + 2 + 4 + 8 + 16 + 32 + 64 + 128 + 256 + 512)
+A for loop can iterate from 1 to 10 and the total sum can be saved in a variable called temp
+
+If options 1 and 2 are similar then the output will be "Both options are similar"
+Otherwise options 1 or 2 will output depending on which option offers a greater pay rate
+
+
+
+
+
+"""
